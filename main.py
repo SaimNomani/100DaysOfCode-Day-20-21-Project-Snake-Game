b73835f134dceeb322bee23df8777db8f5521e54
SaimNomani <nomanisaim591@gmail.com>
2023-10-19 18:53:10 +0500

    I have implemented arrow key control in the game.

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ screen.title("Snake Game 🐍")
 screen.tracer(0)
 
 snake=Snake()
-
+screen.listen()
+screen.onkey(snake.move_up,'Up')
+screen.onkey(snake.move_down,'Down')
+screen.onkey(snake.turn_right,'Right')
+screen.onkey(snake.turn_left,'Left')
 while True:
     screen.update()
     time.sleep(0.1)
--- snake.py
+++ snake.py
@@ -1,12 +1,18 @@
 from turtle import Turtle
 
 INITIAL_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+SNAKE_MOVE_DIST = 10
+UP=90
+DOWN=270
+RIGHT=0
+LEFT=180
 
 
 class Snake:
     def __init__(self):
         self.snake_segments = []
         self.create_snake()
+        self.head_of_snake = self.snake_segments[0]
 
     def create_snake(self):
         for position in INITIAL_POSITIONS:
@@ -21,4 +27,20 @@ class Snake:
             new_x = self.snake_segments[segment - 1].xcor()
             new_y = self.snake_segments[segment - 1].ycor()
             self.snake_segments[segment].goto(new_x, new_y)
-        self.snake_segments[0].forward(10)
+        self.head_of_snake.forward(SNAKE_MOVE_DIST)
+
+    def move_up(self):
+        if self.head_of_snake.heading() != DOWN:
+            self.head_of_snake.setheading(UP)
+
+    def move_down(self):
+        if self.head_of_snake.heading() != UP:
+            self.head_of_snake.setheading(DOWN)
+
+    def turn_right(self):
+        if self.head_of_snake.heading() != LEFT:
+            self.head_of_snake.setheading(RIGHT)
+
+    def turn_left(self):
+        if self.head_of_snake.heading() != RIGHT:
+            self.head_of_snake.setheading(LEFT)
